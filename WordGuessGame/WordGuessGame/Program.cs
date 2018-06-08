9ef324bc5f0032392c37356c1d4259ee9f1e2a15
paulritzman <ritzman.paul@gmail.com>
2018-06-08 10:59:19 -0700

    Initial repo setup; Created methods to write main game menu, and verify user input

---
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WordGuessGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            MainMenu();
+
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("\tWelcome to the \"Guess the Word!\"\n");
+        }
+
+        private static void MainMenu()
+        {
+            Console.WriteLine(
+                "Please select one of the options below:\n" +
+                "1) Play a Game!\n" +
+                "2) Edit Word Bank\n" +
+                "3) Exit\n");
+        }
+
+        public static uint VerifyMainMenuInput(string mainMenuInput)
+        {
+            bool isValid = uint.TryParse(mainMenuInput, out uint numericInput);
+            
+            if (isValid && (numericInput >= 1 && numericInput <= 3))
+            {
+                return numericInput;
+            }
+
+            return 0;
+        }
+    }
+}
